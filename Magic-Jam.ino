1dded0104886e4ab9a34fe07ffcef96af69b0d5d
vxav <xavier.avrillier@gmail.com>
2016-08-29 15:57:02 +0100

    Update Magic-Jam.ino

--- Magic-Jam.ino
+++ Magic-Jam.ino
@@ -1,4 +1,4 @@
-#include <Adafruit_WS2801.h>   // Link to WS2801 library: https://github.com/adafruit/Adafruit-WS2801-Library/blob/master/Adafruit_WS2801.h
+#include <Adafruit_WS2801.h>   // Link to WS2801 library: https://github.com/adafruit/Adafruit-WS2801-Library
 
 int dataPin = 9;     // LED strip data
 int clockPin = 10;   // LED strip clock
@@ -15,6 +15,7 @@ byte TempMax = 30;    // Temperature at which the lamp is completely Red
 byte TempMin = 15;    // Temperature at which the lamp is completely Red
 byte Coefficient = 0; // Coefficient used to convert celsius temperature into a 255 scale value
 int UpdateInt = 2000; // Interval between temperature checks (in ms)
+byte tempCelcius = 0;  // Temperature in degrees Celcius
 
 Adafruit_WS2801 strip = Adafruit_WS2801(numPixel, dataPin, clockPin);
 
@@ -28,7 +29,7 @@ void setup() {
   Coefficient = TempMax - TempMin;
   Coefficient = 255/Coefficient;
   
-  effectPolice();  // Startup effect
+  //effectPolice();  // Startup effect
 }
 
 
@@ -37,24 +38,23 @@ void setup() {
 //////////////////////////////////
 void loop() {
 
-  val = int(Thermistor(analogRead(thermPin)));
-  Temp = Temp * Coefficient;    // Convert to 255 scale: 255 is 30 degrees
+  tempCelcius = int(Thermistor(analogRead(thermPin)));
+  val = tempCelcius * Coefficient;    // Convert to 255 scale: 255 is TempMax and 0 is TempMin
    
-  Serial.print(Temp);
+  Serial.print(tempCelcius);
   Serial.print(" degrees c. -   ");
-	
-	if (temp >= TempMin && temp <= TempMax)
-		if (val > 128) {
-		  setColor(Color(val,255-val,0));
-		} else {
-		  setColor(Color(val,255-val,128-val));
-		}
-	} else if (temp < TempMin) {
-		setColor(Color(0,255,128));
-	} else {
-		setColor(Color(255,0,0));
-	}
-
+  
+  if (tempCelcius >= TempMin && tempCelcius <= TempMax) {
+    if (val > 128) {
+      setColor(Color(val,255-val,0));
+    } else {
+      setColor(Color(val,255-val,128-val));
+    }
+  } else if (tempCelcius < TempMin) {
+    setColor(Color(0,255,128));
+  } else {
+    setColor(Color(255,0,0));
+  }
   delay(UpdateInt);
 }
 
@@ -65,7 +65,6 @@ void loop() {
 double Thermistor(int RawADC) {
  double Temp;
  Temp = log(10000.0*((1024.0/RawADC-1)));
-//         =log(10000.0/(1024.0/RawADC-1)) // for pull-up configuration
  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
  Temp = Temp - 273.15;            // Convert Kelvin to Celcius
 
@@ -101,14 +100,14 @@ uint32_t Color(byte r, byte b, byte g)
 }
 
 
-void effectPolice {
-	
-	strip.setPixelColor(blue,Color(0,0,255));
-	strip.setPixelColor(red,Color(255,0,0));
-	strip.show();
-	
-	if (blue==numPixel){blue = 0;}else{blue++;}
-	if (red==numPixel){red = 0;}else{red++;}
-	
-	delay(125); // (1000ms / 8) meaning it does 2 rounds in 1 second with 4 LEDs
+void effectPolice() {
+  
+  strip.setPixelColor(blue,Color(0,0,255));
+  strip.setPixelColor(red,Color(255,0,0));
+  strip.show();
+  
+  if (blue==numPixel){blue = 0;}else{blue++;}
+  if (red==numPixel){red = 0;}else{red++;}
+  
+  delay(125); // (1000ms / 8) meaning it does 2 rounds in 1 second with 4 LEDs
 }
